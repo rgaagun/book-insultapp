16a8e0aa34ade5cd931d294d46003fd6d90572e2
Raul Gomez Agun <raul.gomez.agun.contractor@bbva.com>
2020-10-02 14:41:21 +0200

    Se corrigen los errores

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -14,19 +14,16 @@ databaseURL += System.getenv("postgresql");
 databaseURL += "/" + System.getenv("insults");
 String username = System.getenv("insult");
 String password = System.getenv("insult");
-Connection connection = DriverManager.getConnection(databaseURL, username,
-password);
+Connection connection = DriverManager.getConnection(databaseURL, username,password);
 if (connection != null) {
-String SQL = "select a.string AS first, b.string AS second, c.string AS noun
-from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
+String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
 Statement stmt = connection.createStatement();
 ResultSet rs = stmt.executeQuery(SQL);
 while (rs.next()) {
 if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
 article = "a";
 }
-theInsult = String.format("Thou art %s %s %s %s!", article,
-rs.getString("first"), rs.getString("second"), rs.getString("noun"));
+theInsult = String.format("Thou art %s %s %s %s!", article,rs.getString("first"), rs.getString("second"), rs.getString("noun"));
 }
 rs.close();
 connection.close();
